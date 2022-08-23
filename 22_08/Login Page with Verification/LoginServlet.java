dcace90b2bc290bf0d3c203cb77a6558c42a510d
vatsa111 <69069130+vatsa111@users.noreply.github.com>
2022-08-23 13:44:35 +0530

    Add files via upload

---
+++ 22_08/Login Page with Verification/LoginServlet.java
@@ -0,0 +1,50 @@
+package com.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String usr=request.getParameter("username");
+		String pwd=request.getParameter("password");
+		PrintWriter out=response.getWriter();
+		out.println("Username is "+usr+" password is "+pwd);
+	}
+		
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
